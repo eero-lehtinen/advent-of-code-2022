62f25b781ff7bae55531a3168d732582a14b06fe
Eero Lehtinen <eero@eerolehtinen.fi>
2022-12-05 14:40:28 +0200

    Day 4

---
+++ 04/src/part2.rs
@@ -0,0 +1,39 @@
+use std::str::FromStr;
+
+struct Range {
+	pub start: i64,
+	pub end: i64,
+}
+
+impl FromStr for Range {
+	type Err = ();
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let (left, right) = s.split_once('-').unwrap();
+		let start = left.parse().unwrap();
+		let end = right.parse().unwrap();
+		Ok(Range { start, end })
+	}
+}
+
+fn main() {
+	let input = include_str!("../input.txt");
+
+	let ranges = input
+		.lines()
+		.map(|line| line.split_once(',').unwrap())
+		.map(|(s1, s2)| (s1.parse::<Range>().unwrap(), s2.parse::<Range>().unwrap()))
+		.collect::<Vec<_>>();
+
+	let overlapping_ranges_count = ranges
+		.iter()
+		.map(|(r1, r2)| {
+			i64::from(
+				r1.start <= r2.start && r1.end >= r2.start
+					|| r2.start <= r1.start && r2.end >= r1.start,
+			)
+		})
+		.sum::<i64>();
+
+	println!("Answer: {overlapping_ranges_count}");
+}
