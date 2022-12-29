092a054eb262d71536029c4f94b5b7ea80c287be
Eero Lehtinen <eero@eerolehtinen.fi>
2022-12-29 13:16:53 +0200

    Day 1 more unsafe

--- 01/src/part2_unsafe_libc.rs
+++ 01/src/part2_unsafe_libc.rs
@@ -1,52 +1,55 @@
 #![allow(clippy::all)]
 #![allow(clippy::pedantic)]
 
-use std::{
-	alloc::{alloc_zeroed, dealloc, Layout},
-	io::Read,
-};
+use std::{mem, os::fd::AsRawFd};
+
+const INPUT_BUF_SIZE: usize = 10_000;
+const CALORIES_SIZE: usize = 250;
 
 /// I want to test out some unsafe code, so I'll abuse it in this file.
 /// My actual solution is in the part2.rs file.
 fn main() {
 	unsafe {
-		let input_layout = Layout::array::<u8>(10_000).unwrap();
-		let input = alloc_zeroed(input_layout);
-		let input_end = input.offset(10_000);
+		let input_file = std::fs::File::open("./01/input.txt").unwrap();
+		let input_buf = libc::malloc(INPUT_BUF_SIZE * mem::size_of::<u8>()) as *mut u8;
+		let read_len = libc::read(
+			input_file.as_raw_fd(),
+			input_buf as *mut libc::c_void,
+			INPUT_BUF_SIZE,
+		);
+		if read_len == -1 {
+			panic!("Error reading input file");
+		}
+		let input_len = read_len;
 
-		let mut input_file = std::fs::File::open("./01/input.txt").unwrap();
-		input_file
-			.read_exact(std::slice::from_raw_parts_mut(input, 10_000))
-			.unwrap();
-
-		let calories_layout = Layout::array::<u32>(250).unwrap();
-		let calories = alloc_zeroed(calories_layout) as *mut u32;
-		let mut cal_idx = 0;
+		let calories = libc::malloc(CALORIES_SIZE * mem::size_of::<u32>()) as *mut u32;
+		let mut cal_len = 0;
 		let mut cur_calories = 0;
-		let mut str = input;
+		let mut str = input_buf;
+		let str_end = input_buf.offset(input_len);
 		loop {
 			str = str.offset(1);
 			if *str >= b'0' && *str <= b'9' {
 				cur_calories *= 10;
 				cur_calories += *str as u32 - 48;
 			} else if *str == b'\n' && *str.offset(-1) == b'\n' {
-				cal_idx += 1;
+				cal_len += 1;
 				cur_calories = 0;
 			} else if *str == b'\n' {
-				*calories.offset(cal_idx) += cur_calories;
+				*calories.offset(cal_len) += cur_calories;
 				cur_calories = 0;
 			}
 
-			if str == input_end {
+			if str == str_end {
 				break;
 			}
 		}
-		dealloc(input, input_layout);
+		libc::free(input_buf as *mut libc::c_void);
 
 		for i in 0..3 {
 			let mut max = 0;
 			let mut max_idx = 0;
-			for j in i..250 {
+			for j in i..cal_len {
 				if *calories.offset(j) > max {
 					max = *calories.offset(j);
 					max_idx = j;
@@ -62,6 +65,6 @@ fn main() {
 			*calories + *calories.offset(1) + *calories.offset(2)
 		);
 
-		dealloc(calories as *mut u8, calories_layout);
+		libc::free(calories as *mut libc::c_void);
 	}
 }
